85edbe9433689b1d8606e153db715563436686a4
yayoimizuha <poko.peko.tomo@icloud.com>
2025-06-25 10:47:55 +0900

    add efficientnetv2_b2

--- backbones/__init__.py
+++ backbones/__init__.py
@@ -35,6 +35,8 @@ def get_model(name, **kwargs):
         return get_timmfrv2('mobilenetv4_conv_medium', batchnorm=False)
     elif name == 'mobilenetv4_hybrid_medium':
         return get_timmfrv2('mobilenetv4_hybrid_medium', batchnorm=False)
+    elif name == 'tf_efficientnetv2_b2':
+        return get_timmfrv2('tf_efficientnetv2_b2', batchnorm=False)
     elif name == 'edgeface_xxs_q':
         model = get_timmfrv2('edgenext_xx_small', batchnorm=False)
         model = torch.quantization.quantize_dynamic(model, qconfig_spec={torch.nn.Linear}, dtype=torch.qint8)
